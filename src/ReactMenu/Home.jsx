9365fa07015e6ecdd3c26df50b6a113eb5b80a09
Vaibhav Patil <65767045+vaibhavpatil5@users.noreply.github.com>
2021-12-02 14:15:22 +0530

    Update Home.jsx

--- src/ReactMenu/Home.jsx
+++ src/ReactMenu/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     return (
         <>
                         
-        <Common name='Hi! ' title=' Wel-come ' info="Photography has become my language to express my thinking,
+        <Common name='Hi!!!' title=' Wel-come ' info="Photography has become my language to express my thinking,
          emotions to people. I love working with people's life style. I love Photography." subtit='Photography is about capturing souls not smiles..' imgsrc={pic} 
          visit="/service" btname="Get Started.."/>
         </>
